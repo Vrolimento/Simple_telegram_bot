bb6f3f72ab19552148729a5d14b761ba495b9e3b
Vrolimento <57906557+Vrolimento@users.noreply.github.com>
2019-11-18 20:12:59 +0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -3,10 +3,10 @@ import telegram
 import time
 
 def main():
-    bot = telegram.Bot(token=<token_id>)
+    bot = telegram.Bot(token=<864236860:AAHffJJoFjbfJ6HyAr3b_YmOzASY5oXvecY>)
     chat_id=<chat_id>
-    message='Hi' 
-    interval=21000
+    message='Hi, i am a new bot of TELEGRAM. I am will post message every 60 sec. Good Bye!' 
+    interval=60
     while True:
         bot.send_message(chat_id=chat_id, text=message, parse_mode=telegram.ParseMode.HTML)
         time.sleep(interval)
